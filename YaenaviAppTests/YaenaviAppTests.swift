f0b098081d7321586de427825a231f4e14a604ec
nwatabou <wataru1.11@gmail.com>
2021-12-12 15:08:12 +0900

    add Project

---
+++ YaenaviAppTests/YaenaviAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  YaenaviAppTests.swift
+//  YaenaviAppTests
+//
+//  Created by nakanishi wataru on 2021/12/12.
+//
+
+import XCTest
+@testable import YaenaviApp
+
+class YaenaviAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
