944a566955213609deb6882dcd2630b1acc1d83d
nwatabou <wataru1.11@gmail.com>
2022-04-10 16:41:53 +0900

    add FeatureInterfaces

--- OperationStatusFeature/Package.swift
+++ OperationStatusFeature/Package.swift
@@ -15,8 +15,7 @@ let package = Package(
             targets: ["OperationStatusFeature"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "FeatureInterfaces", path: "../FeatureInterfaces")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
