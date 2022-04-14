5869f896f50d26c3739db8aa06fe0be2b1c7565f
nwatabou <wataru1.11@gmail.com>
2022-04-14 23:02:14 +0900

    add FeatureProvider

---
+++ FeatureInterfaces/Sources/FeatureInterfaces/FeatureProvider/Builder/FerryRouteListViewBuilderProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  FerryRouteListViewBuilderProtocol.swift
+//  
+//
+//  Created by nakanishi wataru on 2022/04/14.
+//
+
+import SwiftUI
+
+public protocol FerryRouteListViewBuilderProtocol {
+    func build(_ request: FerryRouteListViewRequest) -> AnyView
+}
