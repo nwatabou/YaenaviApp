2d691545f66bc7a237fce74694573c29bb60419a
nwatabou <wataru1.11@gmail.com>
2025-10-26 14:57:47 +0900

    feat: Swift Concurrency対応したメソッドを追加

--- AppCore/Sources/AppCore/API/AneiKankouApi.swift
+++ AppCore/Sources/AppCore/API/AneiKankouApi.swift
@@ -158,4 +158,123 @@ public final class AneiKankouApi: FerryApiProtocol {
     
     task.resume()
   }
+  
+  public func fetchRouteStatuses() async throws -> RouteStatusListResponse {
+    guard let aneiKankouUrl = URL(string: Const.aneiKankouDataUrlString) else {
+      throw URLError(.badURL)
+    }
+    
+    let (data, _) = try await URLSession.shared.data(from: aneiKankouUrl)
+    
+    guard let html = try? HTML(html: data, encoding: .utf8) else {
+      throw URLError(.cannotParseResponse)
+    }
+    
+    let infoText = html.xpath("//div[@class='condition_item_caution']").first?.content
+    let routeStatuses: [RouteStatusResponse] = html
+      .xpath("//div[@class='condition_item_btn']/a")
+      .compactMap { routeStatus in
+        guard let routeName = routeStatus.content else {
+          return nil
+        }
+        
+        switch routeStatus.className {
+        case "operation_normal":
+          return .init(
+            name: routeName,
+            status: "operation_normal"
+          )
+          
+        case "operation_suspension":
+          return .init(
+            name: routeName,
+            status: "operation_suspension"
+          )
+          
+        case "operation_partial":
+          return .init(
+            name: routeName,
+            status: "operation_partial"
+          )
+        default:
+          return nil
+        }
+      }
+    
+    return .init(
+      info: infoText,
+      routeStatuses: routeStatuses
+    )
+  }
+  
+  public func fetchRouteScheduleList(routePrefix: String) async throws -> RouteScheduleListResponse {
+    guard let aneiKankouUrl = URL(string: Const.aneiKankouDataUrlString) else {
+      throw URLError(.badURL)
+    }
+    
+    let (data, _) = try await URLSession.shared.data(from: aneiKankouUrl)
+    
+    guard let html = try? HTML(html: data, encoding: .utf8) else {
+      throw URLError(.cannotParseResponse)
+    }
+    
+    let routes: [RouteScheduleListResponse] = html
+      .xpath("//div[@class='condition_item']")
+      .compactMap { route in
+        let outwardRouteIndex: Int = 0
+        let returnRouteIndex: Int = 1
+        
+        guard let routeName = route.xpath("div[@class='condition_item_title']").first?.content?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let outwardRouteName = route.xpath("div[@class='condition_item_detail flexbox']/div[@class='condition_item_port_title']")[outwardRouteIndex].content?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let returnRouteName = route.xpath("div[@class='condition_item_detail flexbox']/div[@class='condition_item_port_title']")[returnRouteIndex].content?.trimmingCharacters(in: .whitespacesAndNewlines) else {
+          return nil
+        }
+        
+        var outwardSchedules: [RouteScheduleResponse] = []
+        var returnSchedules: [RouteScheduleResponse] = []
+        route.xpath("div[@class='flexbox']/div[@class='condition_item_port_detail']")
+          .enumerated()
+          .forEach { index, scheduleList in
+            scheduleList.xpath("div[@class='flexbox']")
+              .forEach { schedule in
+                switch index {
+                case outwardRouteIndex:
+                  guard let time = schedule.xpath("div[@class='condition_item_port_detail_time']").first?.content,
+                        let statusMark = schedule.xpath("div[@class='condition_item_port_detail_status']").first?.content else { return }
+                  outwardSchedules.append(
+                    .init(
+                      time: time.trimmingCharacters(in: .whitespacesAndNewlines),
+                      status: statusMark.trimmingCharacters(in: .whitespacesAndNewlines)
+                    )
+                  )
+                case returnRouteIndex:
+                  guard let time = schedule.xpath("div[@class='condition_item_port_detail_time']").first?.content,
+                        let statusMark = schedule.xpath("div[@class='condition_item_port_detail_status']").first?.content else { return }
+                  returnSchedules.append(
+                    .init(
+                      time: time.trimmingCharacters(in: .whitespacesAndNewlines),
+                      status: statusMark.trimmingCharacters(in: .whitespacesAndNewlines)
+                    )
+                  )
+                default:
+                  return
+                }
+              }
+          }
+        
+        return .init(
+          name: routeName,
+          outwardRouteName: outwardRouteName,
+          outwardRouteSchedules: outwardSchedules,
+          returnRouteName: returnRouteName,
+          returnRouteSchedules: returnSchedules
+        )
+      }
+    
+    guard let route = routes.first(where: { $0.name.contains(routePrefix) }) else {
+      throw URLError(.resourceUnavailable)
+    }
+    
+    return route
+  }
 }
